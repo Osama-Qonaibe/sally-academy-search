ed7235c270a6b8fd388d20ca007a5262364ae64b
Osama Qonaibe <qoomre@gmail.com>
2026-02-05 04:22:39 +0200

    Fix imports order in chat.ts

--- lib/actions/chat.ts
+++ lib/actions/chat.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+
 import { createClient } from '@/lib/supabase/server'
 import { type Chat } from '@/lib/types'
 
